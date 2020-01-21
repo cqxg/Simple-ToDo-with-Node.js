bfcc0811dfe61dcc146da5c41b39951c5dcc61cf
cqxg <cqxg@yandex.ru>
2020-01-21 16:05:26 +0300

    feat: add hbs engine

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express()
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
-})
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 async function start() {
     try {
