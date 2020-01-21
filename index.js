96b34f8777d05a5aa3853efe762c701169b36b4d
cqxg <cqxg@yandex.ru>
2020-01-21 18:21:39 +0300

    feat: add styles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const todosRoutes = require('./routes/todos');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,6 +17,7 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(todosRoutes);
 
