d534fbb0ca171911aba7b5219c2e6ffcb3bcec42
cqxg <cqxg@yandex.ru>
2020-01-21 16:03:24 +0300

    feat: add handlebars

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
 
-app.listen(PORT, () => {
-    console.log('Server has been started...')
-});
+async function start() {
+    try {
+        await mongoose.connect('', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        });
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        });
+    } catch (e) {
+        console.log(e);
+    };
+};
+
+start();
