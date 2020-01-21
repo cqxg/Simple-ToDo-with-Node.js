bf45b1f2799b170fba4f2e74fa3648760f246a4a
cqxg <cqxg@yandex.ru>
2020-01-21 17:04:42 +0300

    feat: add active link

--- routes/todos.js
+++ routes/todos.js
@@ -3,13 +3,15 @@ const router = Router();
 
 router.get('/', (req, res) => {
     res.render('index', {
-        title: 'Todos list'
+        title: 'Todos list',
+        isIndex: true
     });
 });
 
 router.get('/create', (req, res) => {
     res.render('create', {
-        title: 'Create ToDo'
+        title: 'Create ToDo',
+        isCreate: true
     });
 });
 
