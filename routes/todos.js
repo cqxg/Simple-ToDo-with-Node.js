ce75673f7b344edc45ea2b09bc2a55d72a33c344
cqxg <cqxg@yandex.ru>
2020-01-21 16:47:39 +0300

    feat: add dynamic title

--- routes/todos.js
+++ routes/todos.js
@@ -2,11 +2,15 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Todos list'
+    });
 });
 
 router.get('/create', (req, res) => {
-    res.render('create')
+    res.render('create', {
+        title: 'Create ToDo'
+    });
 });
 
 module.exports = router;
